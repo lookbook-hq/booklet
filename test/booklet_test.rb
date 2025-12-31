35f6b9bb4a51c67c499e9577bbc356959468f170
Mark Perkins <mark@allmarkedup.com>
2025-12-31 15:14:39 +0000

    Fix version test
    Signed-off-by: Mark Perkins <mark@allmarkedup.com>

--- test/booklet_test.rb
+++ test/booklet_test.rb
@@ -4,7 +4,7 @@ module Booklet
   class BookletTest < Minitest::Test
     context ".version" do
       should "return the version number" do
-        assert_equal "0.0.0", Booklet.version
+        assert_equal "0.0.1", Booklet.version
       end
     end
   end
