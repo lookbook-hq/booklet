8a50d3ec4a04dd402e86435ab6a4120ed408b86d
Mark Perkins <mark@allmarkedup.com>
2026-01-28 22:45:18 +0000

    Set page properties from frontmatter values
    Signed-off-by: Mark Perkins <mark@allmarkedup.com>

---
+++ lib/booklet/nodes/concerns/hideable.rb
@@ -0,0 +1,13 @@
+module Booklet
+  module Hideable
+    extend ActiveSupport::Concern
+
+    included do
+      prop :hidden, _Boolean?, writer: :public, reader: :public, default: false
+
+      def hidden?
+        @hidden
+      end
+    end
+  end
+end
